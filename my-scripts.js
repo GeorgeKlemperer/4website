88f404c991347f583b959274a9f8ef2bd7e09cc3
GeorgeKlemperer <george@klemperer.co.uk>
2023-03-21 16:05:49 +0000

    Feat scroll navbar

--- my-scripts.js
+++ my-scripts.js
@@ -1,3 +1,15 @@
 const date = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 document.getElementById("date").innerHTML = days[date.getDay()]+" "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+
+
+// When the user scrolls down 200px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 200) {
+    document.getElementById("scrollnavbar").style.top = "0";
+  } else {
+    document.getElementById("scrollnavbar").style.top = "-50px";
+  }
+}
